dd450a518b60d495b6dae43f728465ea97163d3f
KDY <137839917+Kdy010010@users.noreply.github.com>
2025-04-27 12:25:12 +0900

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,22 @@
 #include "neuix_vga.h"
-#include "neuix_userland"
-// 엔트리 포인트 (부트로더가 호출하는 함수)
-void kernel_main(void) {
-    vga_init();  // 화면 초기화
-    vga_set_color(COLOR_WHITE, COLOR_BLUE);  // 흰 글씨 + 파란 배경
-    vga_write("neuix successfully booted!\n");
-    vga_set_color(COLOR_LIGHT_GREEN, COLOR_BLACK);
-    vga_write("loading userland.\n");
-    userland();
+#include "neuix_keyboard.h"
+#include "neuix_fs.h"
+#include "neuix_userland.h"
 
+void kernel_main() {
+    vga_set_color(COLOR_WHITE, COLOR_BLACK);
+    vga_clear();
+    vga_write("contact to kdywkrrk@gmail.com\n");
+    vga_write("notice that your disk can be busted\n");
+    vga_write("Neuix 1.2 booted\n");
+
+    fs_init();        // 파일시스템 초기화
+    keyboard_init();  // 키보드 초기화 (필요시)
+
+    login();          // 로그인
+    userland();       // 쉘 시작
+
+    while (1) {
+        __asm__ volatile ("hlt");
+    }
 }
